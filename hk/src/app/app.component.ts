03528ec134bee74e948ba1ad4b4616b4b76b2275
Victor Martins <martinsvictor6781@gmail.com>
2022-07-13 09:06:50 -0300

    ajustes nas imagens

--- hk/src/app/app.component.ts
+++ hk/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'Hollow Knight';
   constructor(){
     if(localStorage.getItem('fontSize') !== null){
       updateFontSize()
